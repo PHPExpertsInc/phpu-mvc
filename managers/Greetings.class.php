5e1cd36a66d4cd815a7fe6099df25f4bb70185f3
Theodore R. Smith <theodore@phpexperts.pro>
2011-07-22 13:48:40 -0500

    Cleaned up the license headers.

--- managers/Greetings.class.php
+++ managers/Greetings.class.php
@@ -1,10 +1,12 @@
 <?php
 
-/** The Collabra Market Project
-  *   Part of the Collabra Commercial Collaboration Platform
+/** PHP University's MVC Project
+  *   Part of the PHP University Code Collection
+  *   http://www.phpu.cc/mvc/
   *
   * Copyright(c) 2011 Theodore R. Smith <theodore@phpexperts.pro>
-  * All rights reserved.
+  * License: Creative Commons Attribution License v3.0
+  * http://creativecommons.org/licenses/by/3.0
  **/
 
 class GreetingsManager
