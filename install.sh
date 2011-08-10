1d68f0063e2312ceac1246e4431d49b34101acd7
Theodore R. Smith <theodore@phpexperts.pro>
2011-08-10 12:10:25 -0500

    Created installer script.

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+ln -s lib/phpu-mvc/LICENSE lib/phpu-mvc/MVC.php lib/phpu-mvc/api lib/phpu-mvc/build.xml lib/phpu-mvc/controllers lib/phpu-mvc/install.sh lib/phpu-mvc/managers lib/phpu-mvc/models lib/phpu-mvc/tests lib/phpu-mvc/views lib/phpu-mvc/web .
+ln -s ../lib/phpu-mvc/web/index.php web/index.php
+ln -s ../../lib/phpu-mvc/web/images/logo.png web/images/
+ln -s ../../lib/phpu-mvc/web/css/main.css web/css/
